d390e088ec58b7d88dd65c474d9be16e36d79d32
guillaumeletalle <let.guillaume@gmail.com>
2023-09-04 23:24:18 +0200

    View/Form/Test Client ok

--- src/Form/CTType.php
+++ src/Form/CTType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class CTType extends AbstractType
 {
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
